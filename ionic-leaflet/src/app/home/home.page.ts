6554d7fb646a695e3444b705fffe1eb7dde6bd39
Andy1686 <andyvoiturier.idb@gmail.com>
2018-12-06 11:36:07 +0100

    fin branche geolocIonicNative

--- ionic-leaflet/src/app/home/home.page.ts
+++ ionic-leaflet/src/app/home/home.page.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
-import { DirectionModalComponent } from '../direction-modal/direction-modal.component';
 
 @Component({
   selector: 'app-home',
@@ -11,11 +10,4 @@ export class HomePage implements OnInit {
   constructor(private modalController: ModalController) {}
 
   ngOnInit() {}
-
-  async onFabIteneraireClick() {
-    const modal = await this.modalController.create({
-      component: DirectionModalComponent
-    });
-    return await modal.present();
-  }
 }
