283e59547410f0927ccca3b402eef61a1543e413
Emanuel Peroni <emanuelperoni@gmail.com>
2018-12-09 22:13:58 +0100

    refact binary search

--- server/routes/ors.js
+++ server/routes/ors.js
@@ -12,7 +12,7 @@ router.post("/", async function(req, res, next) {
    */
   try {
     var json = req.body.coordinates;
-    let startIcr = graph.closestEntryToNetwork(json[0], 100, 7);
+    let startIcr = graph.closestEntryToNetwork(json[0], 100, 6);
     let endIcr = graph.closestEntryToNetwork(json[1], 100, 7);
     let geoJsonStart = await ors.calculate([json[0], startIcr]);
     geoJsonStart = geoJsonStart.features[0];
