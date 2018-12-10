32399a4e7c85438b83f5520da92b993375325fee
guillaume <g@w.x>
2018-12-10 20:28:40 +0100

    debug affichage

--- ionic-leaflet/src/app/map-box/map-box.component.ts
+++ ionic-leaflet/src/app/map-box/map-box.component.ts
@@ -43,58 +43,14 @@ export class MapBoxComponent implements OnInit {
     } else {
       this.map = this.initializingMap("light");
     }
-    //
-    // Display route
-    this.mapRouteService.routeSubject.subscribe(geojson => {
-        console.log("subscribe")
-        console.log(geojson)
-        //hide ICR layer
-        this.map.setLayoutProperty(this.icrLayerID,'visibility','none');
-        //check if current route layer exists and remove if exists
-        if(this.map.getLayer(this.routeLayerID) !== undefined){
-            this.map.removeLayer(this.routeLayerID).removeSource(this.routeLayerID);
-        }
-        //display new route layer
-        this.displayGeoJson(geojson,this.map,this.routeLayerID);
   
-        if (this.startpoint !== undefined) this.startpoint.remove();
-        if (this.endpoint !== undefined) this.endpoint.remove();
-  
-        var features = geojson["features"];
-        var coordinatesFirstFeature = features[0]["geometry"]["coordinates"];
-        var coordinatesLastFeature =
-          features[features["length"] - 1]["geometry"]["coordinates"];
-        var lengthLF = coordinatesLastFeature["length"];
-  
-        var latStart=coordinatesFirstFeature[0][1];
-        var longStart=coordinatesFirstFeature[0][0];
-        var latEnd=coordinatesLastFeature[lengthLF - 1][1];
-        var longEnd=coordinatesLastFeature[lengthLF - 1][0];
-        this.startpoint = this.addPointToMap(
-          latStart,
-          longStart,
-          "../assets/marker/start.png"
-        );
-        this.endpoint = this.addPointToMap(
-          latEnd,
-          longEnd,
-          "assets/marker/end.png"
-        );
-        var coordinates=[[longStart,latStart],[longEnd,latEnd]];
-        var bounds =coordinates.reduce(function(bounds,coord){
-            return bounds.extend(coord);
-        },new mapboxgl.LngLatBounds(coordinates[0],coordinates[0]));
-        this.map.fitBounds(bounds,{
-            padding:200
-        });
-   
-    });
+    
     console.log(this.map)
 
   }
 
   initializingMap(mapStyle) {
-      console.log("initialize")
+    console.log("initialize")
     var map = new mapboxgl.Map({
       container: "map",
       style: "mapbox://styles/mapbox/" + mapStyle + "-v9",
@@ -111,11 +67,57 @@ export class MapBoxComponent implements OnInit {
       mapCanvas["style"].width = "100%";
       map.resize();
 
-      map=this.displayICRWithColors(map);
+      
     });   
-    
-    return map;
-  }
+    this.map=map=this.displayICRWithColors(map);
+    // Display route
+    this.mapRouteService.routeSubject.subscribe(geojson => {
+      console.log("subscribe")
+      console.log(geojson)
+      //hide ICR layer
+      this.map.setLayoutProperty(this.icrLayerID,'visibility','none');
+      //check if current route layer exists and remove if exists
+      if(this.map.getLayer(this.routeLayerID) !== undefined){
+          this.map.removeLayer(this.routeLayerID).removeSource(this.routeLayerID);
+      }
+      //display new route layer
+      this.displayGeoJson(geojson,this.map,this.routeLayerID);
+
+      if (this.startpoint !== undefined) this.startpoint.remove();
+      if (this.endpoint !== undefined) this.endpoint.remove();
+
+      var features = geojson["features"];
+      var coordinatesFirstFeature = features[0]["geometry"]["coordinates"];
+      var coordinatesLastFeature =
+        features[features["length"] - 1]["geometry"]["coordinates"];
+      var lengthLF = coordinatesLastFeature["length"];
+
+      var latStart=coordinatesFirstFeature[0][1];
+      var longStart=coordinatesFirstFeature[0][0];
+      var latEnd=coordinatesLastFeature[lengthLF - 1][1];
+      var longEnd=coordinatesLastFeature[lengthLF - 1][0];
+      this.startpoint = this.addPointToMap(
+        latStart,
+        longStart,
+        "../assets/marker/start.png"
+      );
+      this.endpoint = this.addPointToMap(
+        latEnd,
+        longEnd,
+        "assets/marker/end.png"
+      );
+      var coordinates=[[longStart,latStart],[longEnd,latEnd]];
+      var bounds =coordinates.reduce(function(bounds,coord){
+          return bounds.extend(coord);
+      },new mapboxgl.LngLatBounds(coordinates[0],coordinates[0]));
+      this.map.fitBounds(bounds,{
+          padding:200
+      });
+
+  });
+
+  return map;
+}
 
   addPointToMap(lat:number,long:number,iconUrl:string){
     /*var el =document.createElement('div');
@@ -150,11 +152,13 @@ export class MapBoxComponent implements OnInit {
   }
 
   displayICRWithColors(map) {
-
+    /*this.http
+    .get<GeoJsonObject>("assets/latlong_icr.json")
+    .subscribe(geojson => {this.displayGeoJson(geojson,map,this.icrLayerID)});*/
     this.http
       .get<any>('assets/icr-with-colors.json')
       .toPromise()
       .then(geojson => this.displayGeoJson(geojson,map,this.icrLayerID));
-     return map; 
+      return map
   }
 }
--- ionic-leaflet/src/app/services/map-route.service.ts
+++ ionic-leaflet/src/app/services/map-route.service.ts
@@ -19,8 +19,8 @@ export class MapRouteService {
 
     this.httpClient
       .post<GeoJsonObject>(
-        //'http://test-dockerfull-env-2.xgpz6fryfk.eu-west-1.elasticbeanstalk.com/api/ors-directions',
-        "http://localhost:8081/api/ors-directions",
+        'http://test-dockerfull-env-2.xgpz6fryfk.eu-west-1.elasticbeanstalk.com/api/ors-directions',
+        //"http://localhost:8081/api/ors-directions",
         json
       )
       .toPromise()
