21e8f1289e5f9b3b678ba170bbdae1c6485c5099
Vincent Van Rossem <vincent@172-15-1-48.lightspeed.stlsmo.sbcglobal.net>
2018-12-10 13:05:41 +0100

    fixed icr colorisation

--- ionic-leaflet/src/app/map-box/map-box.component.ts
+++ ionic-leaflet/src/app/map-box/map-box.component.ts
@@ -59,73 +59,9 @@ export class MapBoxComponent implements OnInit {
 
   displayICRWithColors(map) {
     this.http
-      .get<any>("assets/latlong_icr.json")
+      .get<any>("assets/icr-with-colors.json")
       .toPromise()
       .then(data => {
-        var color = null;
-        data.features.forEach(element => {
-          switch (element.properties.icr) {
-            case "1":
-              color = "#00cc00";
-              break;
-            case "2":
-              color = "#99b3ff";
-              break;
-            case "3":
-              color = "#FF3232";
-              break;
-            case "4":
-              color = "#9932FF";
-              break;
-            case "5":
-              color = "#00510A";
-              break;
-            case "6":
-              color = "#00510A";
-              break;
-            case "7":
-              color = "#D12200";
-              break;
-            case "8":
-              color = "#00cc00";
-              break;
-            case "9":
-              color = "#9932FF";
-              break;
-            case "10":
-              color = "#FF3232";
-              break;
-            case "11":
-              color = "#187c00";
-              break;
-            case "12":
-              color = "#00510A";
-              break;
-            case "A":
-              color = "#FF8205";
-              break;
-            case "B":
-              color = "#FF8205";
-              break;
-            case "C":
-              color = "#FF8205";
-              break;
-            case "CK":
-              color = "#05AFFF";
-              break;
-            case "SZ":
-              color = "#05AFFF";
-              break;
-            case "MM":
-              color = "#00256B";
-              break;
-            case "PP":
-              color = "#D12200";
-              break;
-          }
-          element.properties["color"] = color;
-        });
-
         map.addLayer({
           id: "all_icr",
           type: "line",
