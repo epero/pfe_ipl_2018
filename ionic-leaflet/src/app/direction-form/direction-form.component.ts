b6c5ad4e41cefbc96f6d684aec8e38ed5afab9c7
Vincent Van Rossem <vincent@Vincents-MacBook-Air.local>
2018-12-10 21:01:52 +0100

    geocoding with pages and service communication

--- ionic-leaflet/src/app/direction-form/direction-form.component.ts
+++ ionic-leaflet/src/app/direction-form/direction-form.component.ts
@@ -1,101 +1,75 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { MapRouteService } from '../services/map-route.service';
-import { AddressesService } from '../services/addresses.service';
-import { LoadingController } from '@ionic/angular';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { HttpClient } from "@angular/common/http";
+import { MapRouteService } from "../services/map-route.service";
+import { AddressesService } from "../services/addresses.service";
+import { LoadingController, NavController } from "@ionic/angular";
 
 @Component({
-  selector: 'app-direction-form',
-  templateUrl: './direction-form.component.html',
-  styleUrls: ['./direction-form.component.scss']
+  selector: "app-direction-form",
+  templateUrl: "./direction-form.component.html",
+  styleUrls: ["./direction-form.component.scss"]
 })
 export class DirectionFormComponent implements OnInit {
   startInput: String;
   startJSON: any;
-  startSearch: boolean;
   endInput: String;
   endJSON: any;
-  endSearch: boolean;
-  startAddrList: Array<any>;
-  endAddrList: Array<any>;
   deployed: boolean;
   haveRoute: boolean;
   loader: HTMLIonLoadingElement;
 
   constructor(
     private httpClient: HttpClient,
+    private route: ActivatedRoute,
     private mapRouteService: MapRouteService,
     private addressesService: AddressesService,
-    private loadingController: LoadingController
+    private loadingController: LoadingController,
+    private navController: NavController
   ) {
-    this.startAddrList = [];
-    this.endAddrList = [];
     this.deployed = true;
     this.haveRoute = false;
   }
 
   ngOnInit() {
+    this.addressesService.addressSubject.subscribe(addr => {
+      switch (addr["id"]) {
+        case "start":
+          this.startJSON = addr;
+          break;
+        case "end":
+          this.endJSON = addr;
+          break;
+      }
+    });
+
     this.mapRouteService.routeSubject.subscribe(() => {
       this.deployed = false;
       this.haveRoute = true;
       this.removeLoader();
     });
     this.startInput =
-      '75, Rue François Gay, Woluwe-Saint-Pierre, Bruxelles-Capitale, 1150, Belgique';
+      "75, Rue François Gay, Woluwe-Saint-Pierre, Bruxelles-Capitale, 1150, Belgique";
     this.endInput =
-      'Clos Chapelle-aux-Champs, Woluwe-Saint-Lambert, Bruxelles-Capitale, 1200, Belgique';
+      "Clos Chapelle-aux-Champs, Woluwe-Saint-Lambert, Bruxelles-Capitale, 1200, Belgique";
   }
 
-  onStartInputChange() {
-    if (this.startSearch) {
-      this.addressesService.getAddresses(this.startInput).then(addresses => {
-        this.startAddrList = Object.keys(addresses).map(key => addresses[key]);
-      });
-    }
+  onStartInputClick() {
+    this.navController.navigateForward("/search/start", true);
   }
 
-  onStartInputFocus() {
-    this.startSearch = true;
-  }
-
-  onStartInputBlur() {
-    this.startSearch = false;
-  }
-
-  onStartAddrItemClick(addr) {
-    this.startAddrList = [];
-    this.startJSON = addr;
-  }
-
-  onEndInputChange() {
-    if (this.endSearch) {
-      this.addressesService.getAddresses(this.endInput).then(addresses => {
-        this.endAddrList = Object.keys(addresses).map(key => addresses[key]);
-      });
-    }
-  }
-
-  onEndInputFocus() {
-    this.endSearch = true;
-  }
-
-  onEndInputBlur() {
-    this.endSearch = false;
-  }
-
-  onEndAddrItemClick(addr) {
-    this.endAddrList = [];
-    this.endJSON = addr;
+  onEndInputClick() {
+    this.navController.navigateForward("/search/end", true);
   }
 
   onSearchRouteBtnClick() {
     //send coordinates to backend
     if (this.startJSON !== undefined && this.endJSON !== undefined) {
       this.mapRouteService.sendCoordinatesToServer(
-        this.startJSON['x'],
-        this.startJSON['y'],
-        this.endJSON['x'],
-        this.endJSON['y']
+        this.startJSON["x"],
+        this.startJSON["y"],
+        this.endJSON["x"],
+        this.endJSON["y"]
       );
       this.afficheLoader();
     }
@@ -111,10 +85,10 @@ export class DirectionFormComponent implements OnInit {
 
   async afficheLoader() {
     this.loader = await this.loadingController.create({
-      spinner: 'dots',
-      message: 'Chargement de la route',
+      spinner: "dots",
+      message: "Chargement de la route",
       animated: true,
-      id: 'routeLoader',
+      id: "routeLoader",
       showBackdrop: true,
       translucent: true
     });
