63f49b86842aeb0eee32228b3b035040f0ba0218
guillaume <g@w.x>
2018-12-11 13:01:17 +0100

    gestion erreurs partie 1

--- ionic-leaflet/src/app/services/map-route.service.ts
+++ ionic-leaflet/src/app/services/map-route.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Subject } from 'rxjs';
 import { GeoJsonObject } from 'geojson';
+import { AlertController } from '@ionic/angular';
 
 @Injectable({
   providedIn: 'root'
@@ -10,14 +11,14 @@ export class MapRouteService {
   routeSubject: Subject<GeoJsonObject>;
   route: GeoJsonObject;
 
-  constructor(private httpClient: HttpClient) {
+  constructor(private httpClient: HttpClient,private alertController:AlertController) {
     this.routeSubject = new Subject<GeoJsonObject>();
   }
 
   sendCoordinatesToServer(startLong, startLat, endLong, endLat) {
     let json = { coordinates: [[startLong, startLat], [endLong, endLat]] };
 
-    this.httpClient
+    return this.httpClient
       .post<GeoJsonObject>(
         'http://test-dockerfull-env-2.xgpz6fryfk.eu-west-1.elasticbeanstalk.com/api/ors-directions',
         //"http://localhost:8081/api/ors-directions",
@@ -29,9 +30,27 @@ export class MapRouteService {
       })
       .catch(error => {
         console.log(error);
+        switch(error.status){
+          case 404:
+          this.presentAlert("Itinéraire non trouvé", "L'itinéraire introduit n'existe pas ou est en dehors des limites de la zone de recherche.");
+          break;
+          default:
+          this.presentAlert("Service indisponible !", "Le service est temporairement indisponible, réessayez plus tard.");
+
+        }
+        return error;
       });
   }
 
+  async presentAlert(title,message) {
+    const alert = await this.alertController.create({
+      header: title,
+      message: message,
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
   setRoute(route: GeoJsonObject) {
     this.route = route;
     this.emitRoute();
