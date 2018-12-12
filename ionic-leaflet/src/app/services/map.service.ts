441343f3bbc1d556d0d377bd076ac37d34fdb27c
Vincent Van Rossem <vincent@172-15-1-48.lightspeed.stlsmo.sbcglobal.net>
2018-12-12 13:12:19 +0100

    fix geolocation in result list

--- ionic-leaflet/src/app/services/map.service.ts
+++ ionic-leaflet/src/app/services/map.service.ts
@@ -1,30 +1,60 @@
-import { Injectable } from '@angular/core';
-import { Subject } from 'rxjs';
+import { Injectable } from "@angular/core";
+import { Subject } from "rxjs";
+import { Geolocation, Geoposition } from "@ionic-native/geolocation/ngx";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class MapService {
   TOKEN: string;
 
   couleur: string;
   resizeSubject: Subject<any>;
+  positionSubject: Subject<Geoposition>;
+  position: Geoposition;
 
-  constructor() {
+  constructor(private geoloc: Geolocation) {
     this.TOKEN =
-      'pk.eyJ1IjoieGRhcmthIiwiYSI6ImNqcGgxdXBobjByNHUza3BkbGtvMGY2eTUifQ.WuwZ_XI2zNxxObLi6moULg';
+      "pk.eyJ1IjoieGRhcmthIiwiYSI6ImNqcGgxdXBobjByNHUza3BkbGtvMGY2eTUifQ.WuwZ_XI2zNxxObLi6moULg";
 
     const n = new Date().getHours();
     if (n >= 18 || n <= 6) {
-      this.couleur = 'dark';
+      this.couleur = "dark";
     } else {
-      this.couleur = 'light';
+      this.couleur = "light";
     }
 
     this.resizeSubject = new Subject<any>();
+    this.positionSubject = new Subject<Geoposition>();
+    this.geoloc
+      .getCurrentPosition({ enableHighAccuracy: true })
+      .then(position => {
+        this.position = position;
+        this.watchLocation();
+      })
+      .catch(err => console.log("Localisation inactive"));
   }
 
   emitResize() {
     this.resizeSubject.next(1);
+  }
+
+  watchLocation() {
+    this.geoloc.watchPosition().subscribe(position => {
+      this.position = position;
+      this.emitPosition();
+    });
+  }
+
+  emitPosition() {
+    this.positionSubject.next(this.position);
+  }
+
+  getPosition() {
+    return this.position;
+  }
+
+  hasPosition() {
+    return this.position !== undefined;
   }
 }
