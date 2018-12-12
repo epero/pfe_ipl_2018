8ee9a2317ccf30de42acfc171c4a5a649a263bd5
Emanuel Peroni <emanuelperoni@gmail.com>
2018-12-13 00:01:08 +0100

    fixed tests

--- server/tests/shortest_path_tests.js
+++ server/tests/shortest_path_tests.js
@@ -4,7 +4,9 @@ chai.use(chaiAsPromised).should();
 const folder = "./geojsons/";
 const config = require("../config.json");
 const graph = require("../my_modules/graph");
-const config = require("../config.json");
+const shortest_path = require("../my_modules/shortest_path");
+const path_2_geojson = require("../my_modules/path_2_geojson");
+
 const test1 = require(folder + config.test._1.file);
 const test2 = require(folder + config.test._2.file);
 
@@ -14,26 +16,30 @@ beforeEach("Setting up the graph", function() {
 
 describe(config.test._1.description, function() {
   it(config.test._1.message, function() {
-    return graph
-      .calculate({
-        source_long: config.test._1.source_long,
-        source_lat: config.test._1.source_lat,
-        dest_long: config.test._1.dest_long,
-        dest_lat: config.test._1.dest_lat
-      })
+    path_2_geojson
+      .convert(
+        shortest_path.calculate({
+          source_long: config.test._1.source_long,
+          source_lat: config.test._1.source_lat,
+          dest_long: config.test._1.dest_long,
+          dest_lat: config.test._1.dest_lat
+        })
+      )
       .should.deep.equal(test1);
   });
 });
 
 describe(config.test._2.description, function() {
   it(config.test._2.message, function() {
-    return graph
-      .calculate({
-        source_long: config.test._2.source_long,
-        source_lat: config.test._2.source_lat,
-        dest_long: config.test._2.dest_long,
-        dest_lat: config.test._2.dest_lat
-      })
+    path_2_geojson
+      .convert(
+        shortest_path.calculate({
+          source_long: config.test._2.source_long,
+          source_lat: config.test._2.source_lat,
+          dest_long: config.test._2.dest_long,
+          dest_lat: config.test._2.dest_lat
+        })
+      )
       .should.deep.equal(test2);
   });
 });
