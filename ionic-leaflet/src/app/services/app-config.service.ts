45b87c6c3c8980f9b3a277eb365368d62647334c
Vincent Van Rossem <vincent@Vincents-MacBook-Air.local>
2018-12-12 23:50:37 +0100

    added app-config for frontend

---
+++ ionic-leaflet/src/app/services/app-config.service.ts
@@ -0,0 +1,30 @@
+export let config = {
+  osm: {
+    countrycodes: "be",
+    addressdetails: "1",
+    viewbox:
+      "4.259079821011285,50.763884731114281,4.481428372975057,50.914058111020985",
+    bounded: "1"
+  },
+  communes: {
+    1000: "Bruxelles-Ville",
+    1030: "Schaerbeek",
+    1040: "Etterbeek",
+    1050: "Ixelles",
+    1060: "Saint-Gilles",
+    1070: "Anderlecht",
+    1080: "Molenbeek-St-Jean",
+    1081: "Koekelberg",
+    1082: "Berchem-Ste-Agathe",
+    1083: "Ganshoren",
+    1090: "Jette",
+    1140: "Evere",
+    1150: "Woluwé-St-Pierre",
+    1160: "Auderghem",
+    1170: "Watermael-Boitsfort",
+    1180: "Uccle",
+    1190: "Forest",
+    1200: "Woluwé-St-Lambert",
+    1210: "St Josse-ten-Noode"
+  }
+};
